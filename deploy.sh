41edc9a2c4e6482bd0eca19a9ffce6a99a1c9aa3
crystal <crystal@legenddigital.cn>
2021-05-26 15:09:20 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -228,7 +228,8 @@ commit+push() {
   if [ $GH_TOKEN ]; then
     # deploy by Travis CI
     # add github token
-    repo="https://"$GH_TOKEN"@github.com/huobiapi/docs.git"
+    # repo="https://"$GH_TOKEN"@github.com/huobiapi/docs.git"
+    repo = "git@github.com:YXQ212526/plagiarism.git"
     git remote add origin-pages $repo
     git push --quiet origin-pages $deploy_branch
   else
